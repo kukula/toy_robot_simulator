47a1b9c140b9f5c6a78be44a89c997cc05019a22
Anatoliy Kukul <anatolkukula@gmail.com>
2016-03-13 16:38:34 +0100

    Add ToyRobotSimulator.run and acceptance specs

--- lib/toy_robot_simulator.rb
+++ lib/toy_robot_simulator.rb
@@ -3,4 +3,11 @@ require "toy_robot_simulator/bot"
 require "toy_robot_simulator/controller"
 
 module ToyRobotSimulator
+  def self.run(file)
+    bot = Bot.new
+    controller = Controller.new(bot)
+    file.read.each_line do |command|
+      controller.perform(command)
+    end
+  end
 end
--- spec/toy_robot_simulator_spec.rb
+++ spec/toy_robot_simulator_spec.rb
@@ -4,4 +4,17 @@ describe ToyRobotSimulator do
   it 'has a version number' do
     expect(ToyRobotSimulator::VERSION).not_to be nil
   end
+
+  describe ".run" do
+    ["0,1,NORTH", "0,0,WEST", "3,3,NORTH"].each_with_index do |expected, i|
+      specify "Acceptance example ##{i}" do
+        filename = File.join File.dirname(__FILE__), "examples", "example_#{i}"
+        file = File.open(filename)
+
+        expect($stdout).to receive(:puts).with(expected)
+
+        ToyRobotSimulator.run(file)
+      end
+    end
+  end
 end
